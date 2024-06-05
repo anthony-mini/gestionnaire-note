143e3193e1aeecf3362d881ab011091b07c78628
anthony-mini <anthony-mini@outlook.fr>
2024-06-05 14:51:43 +0200

    chore: Add AddNote component and NoteContext for managing notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css';
-import { Header } from './components/Header/Header';
+import { Header } from './Header';
+import { NoteProvider } from './NoteContext';
+import AddNote from './AddNotes';
+import NoteList from './NoteList';
 
 function App() {
   return (
     <>
       <Header />
+      <NoteProvider>
+        <div className="container mx-full p-4">
+          <AddNote />
+          <NoteList />
+        </div>
+      </NoteProvider>
     </>
   );
 }
